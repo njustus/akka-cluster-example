6800e119ecf5a212df9b30348b3dca68ee73da81
N. Justus <n-justus@outlook.com>
2022-02-12 18:31:20 +0100

    print which main starts

--- src/main/scala/njustus/clusterexample/Main.scala
+++ src/main/scala/njustus/clusterexample/Main.scala
@@ -3,8 +3,10 @@ package njustus.clusterexample
 object Main {
   def main(args: Array[String]): Unit = {
     if(args.contains("remote")) {
+      println("starting clustered system")
       ClusteredMain.main(args)
     } else {
+      println("starting local system")
       LocalMain.main(args)
     }
   }
