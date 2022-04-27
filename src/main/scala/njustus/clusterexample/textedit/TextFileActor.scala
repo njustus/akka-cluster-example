6ec13981e5ff4b31fb6a8875a3c6c7eccc25a1f0
N. Justus <n-justus@outlook.com>
2022-04-27 09:16:26 +0200

    clear TextFileActor for demo

--- src/main/scala/njustus/clusterexample/textedit/TextFileActor.scala
+++ src/main/scala/njustus/clusterexample/textedit/TextFileActor.scala
@@ -13,37 +13,10 @@ class TextFileActor(interpreter: TextEditorInterpreter,
 
   //message handling
   override def receive: Receive = {
-    case TextEditingProtocol.Join =>
-      val peer = sender()
-      editingPeers.append(peer)
-      log.info("{} joins - {} editors", peer, editingPeers.size)
-
-      peer.tell(TextEditingProtocol.TextFileUpdate(currentTextFile, context.self), context.self)
-    case TextEditingProtocol.Leave =>
-      val peer = sender()
-      val peerIdx = editingPeers.indexOf(peer)
-      if (peerIdx > -1) {
-        editingPeers.remove(peerIdx)
-        log.info("{} leaves - {} editors", peer, editingPeers.size)
-      }
-
-    case editLine: TextEditingProtocol.EditLine =>
-      val peer = sender()
-      val patch = EditPatch(editLine.content, Some(peer.path.name))
-      log.info("edit received {}", editLine)
-
-      val newTextFile = interpreter.editLine(currentTextFile, editLine.lineNo, patch)
-      updateTextFile(newTextFile, peer)
+    case x:Int => log.error("unhandled")
   }
 
   private def updateTextFile(newTextFile: TextFile, editPeer: ActorRef): Unit = {
-    currentTextFile = newTextFile
-
-    val textFileUpdate = TextEditingProtocol.TextFileUpdate(newTextFile, editPeer)
-    log.debug("editing patch applied, notify peers")
-    editingPeers.foreach { actor =>
-      actor.tell(textFileUpdate, context.self)
-    }
   }
 }
 
